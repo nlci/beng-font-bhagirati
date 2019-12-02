f7769c3da4532b1ca2362339d9fec3f142ae6507
Bobby de Vos <bobby_devos@sil.org>
2019-12-02 13:13:23 -0700

    Cleanup of import of common characters

--- tools/encoding.py
+++ tools/encoding.py
@@ -8,10 +8,8 @@ ufo = sys.argv[1]
 font = OpenFont(ufo)
 
 # Modify UFO
-double = font['doubledanda']
-double.name = 'dandadbl'
 
 # Save UFO
-font.changed()
-font.save()
-font.close()
+# font.changed()
+# font.save()
+# font.close()
